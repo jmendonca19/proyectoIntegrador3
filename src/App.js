3aded7760f906d8f4374da759132309f5893d8b1
jmendonca19 <jmendonca@udesa.edu.ar>
2022-09-08 09:29:06 -0300

    nav y rutas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
-
 import Header from './components/Header/Header';
+import {Switch, Route} from 'react-router-dom';
+import Home from './pages/Home/Home';
+
 
 function App() {
   return (
     <>
-    <Header />
+       <Header />
+    <Switch>
+        <Route path="/" exact  >   
+           <Home/> 
+        </Route> 
+    </Switch>
+       <Footer />
+
     
-    <Footer />
     </>
   );
 }
 
 export default App;
+
+
+
+
+
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,21 @@
 import React from 'react'
+import Navigator from '../Nav/Nav'
 import "./Header.css"
 
 export default function Header() {
   return (
     <>
-        <header className="header">
+    <header className="header">
         <div className="contenedor">
-            <a><h2 className="logotipo">pelisYa</h2></a>
+            <h2 className="logotipo">pelisYa</h2>
             <div className="buscador">
                 <form method="GET">
                         <input className= "search" type="text" name="buscador" placeholder="Buscador..." value="" />
                         <button type="submit"><i className="fas fa-search"></i></button>
                 </form>
-            </div>
-
-            <nav className="nav-bar">
-                <ul className="listaNav">
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="#Series">Series</a></li>
-                    <li><a href="#Peliculas">Peliculas</a></li>
-                    <li><a href="favorite.html">Favoritos</a></li>
-                </ul>
-            </nav>
+            </div> 
         </div>
+        <Navigator/>
     </header>
     </>
   )
