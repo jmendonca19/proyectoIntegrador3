7e001271060954cd903c813e94eff11acf07318d
Joaquin <joaquinknez@MacBook-Pro-de-Joaquin-2.local>
2022-09-12 09:23:12 -0300

    detalle pelicula y serie inicio

---
+++ src/pages/DetalleSerie/DetailSerie.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+class DetailSerie extends Component {
+      
+    constructor (){
+        super();
+        this.state = {
+            data: {}
+        }
+    }
+
+ componentDidMount(){
+        const url = "https://api.themoviedb.org/3/movie/${id}?api_key=924a6f16470b17afdd20524ec31c09be"
+        fetch(url)
+            .then((res)=> res.json())
+            .then(datos =>{ 
+                    console.log(datos)
+                    return this.setState({
+                    peliculas: datos.results
+                })
+            })
+            .catch( err => console.log(err))
+     }
+
+     render(){
+        return(
+            <>
+                <img> </img>
+                <h1>{this.state.data.name}</h1>
+                <p> Calificacion: {this.state.data.vote_average} </p>
+                <p> Fecha de estreno: {this.state.data.first_air_date} </p>
+                <p> Sinopsis: {this.state.data.overview} </p>
+                <p> Genero: {this.state} </p>
+                <button> </button>
+
+            </>
+        )
+     }
+
+}
+
+
+export default DetailSerie;
