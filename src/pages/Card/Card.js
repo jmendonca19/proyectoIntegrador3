f692254accedaab02eeaaf6ae7db210c64f84253
ivanfischman <ivanfischman@gmail.com>
2022-09-12 22:52:27 -0300

    favss

--- src/pages/Card/Card.js
+++ src/pages/Card/Card.js
@@ -2,10 +2,10 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import "./Card.css"
 
-function Card({pelicula, props}) {
+function Card(props) {
     const img = 'https://image.tmdb.org/t/p/w342'; /* Primer parte de la url de la imagen, se complementa con el poster_path */
 
-    let {poster_path, title, id} = pelicula
+    let {poster_path, title, id} = props.pelicula
 
   return (
     <> 
@@ -14,7 +14,7 @@ function Card({pelicula, props}) {
             <h2>{title}</h2>
             
             <button className="boton"><Link to={`/detallePelicula/id/${id}`} className="detalle">Detalle</Link></button>
-            <button className="boton" onClick={()=>{props.favorito(pelicula)}}>Favoritos</button>
+            <button className="boton" onClick={()=>{props.favorito(props.pelicula)}}>Favoritos</button>
         </article>
     </>
   )
--- src/pages/Favoritos/Favoritos.js
+++ src/pages/Favoritos/Favoritos.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Card from '../Card/Card';
-
+import "./Favoritos.css"
 class Favoritos extends Component {
     constructor(){
         super()
@@ -10,19 +10,27 @@ class Favoritos extends Component {
     }
 
     componentDidMount(){
+        console.log(this.state.favoritos);
         this.setState({favoritos: JSON.parse(localStorage.getItem('favoritos'))})
     }
 
   render() {
-    return (
-      <div className='peliculas'>
-        {this.state.favoritos.map( pelicula => (
-            <Card 
-                key={pelicula.id} 
-                pelicula={pelicula}
-            />
-        ))}
-      </div>
+      return (
+        <>
+            <h2 className="tituloFavoritos">Tus Peliculas Favoritas</h2>
+            <div className='peliculas'>
+                { this.state.favoritos != 0 ? (
+                    this.state.favoritos.map( pelicula => (
+                        <Card 
+                            key={pelicula.id} 
+                            pelicula={pelicula}
+                        />
+                    ))) : (
+                        <h2 className="tituloFavoritos">No tenes peliculas favoritas seleccionadas</h2>
+                    ) 
+                } 
+            </div>
+        </>
     )
   }
 }
